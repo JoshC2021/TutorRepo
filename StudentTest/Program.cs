0d7ffb32e79c8dd4b5ac49af3d8e638750fdfacc
Josh <thejosh2021@gmailcom>
2021-06-15 17:43:11 -0400

    added student class

---
+++ StudentTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StudentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Student s = new Student("Josh","Carolin");
+            Console.WriteLine(s.Score);
+        }
+    }
+}
---
+++ StudentTest/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentTest
+{
+    class Student
+    {
+        public string FirstName { get; set; } = "John";
+
+        public string LastName { get; set; } = "Doe";
+
+        public double Score { get; set; } = 100;
+
+        public Student()
+        {
+            FirstName = "John";
+            LastName = "Doe";
+            Score = 100;
+        }
+
+        public Student(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public Student(string FirstName, string LastName, double Score) // information you obtained from user/database/etc
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Score = Score;
+        }
+
+        /*
+         *  stirng name = Console.ReadLine();
+         *  
+         *  Student s = new Student(name,"Doe", 12);
+         *  
+         *  
+         */
+
+    }
+}
